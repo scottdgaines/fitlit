fcd9b1347b04d02a676861850bf9608ed7881724
Marianne Barton <mhood24@gmail.com>
2022-08-28 12:52:31 -0600

    Add beginning structure to UserRepository class

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,15 @@
 class UserRepository {
+  constructor (data){
+    this.userData = data;
+  };
+  returnUserData(id){
+    let userInfo = this.userData.find(user => id === user.id)
+    return userInfo
+  };
 
-}
+  returnAllUserStepGoal(){
+
+  }
+};
 
 export default UserRepository;
