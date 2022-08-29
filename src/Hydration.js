9e2ae75ba96a438c93c3a7dbf107040c3923f18e
Scott Gaines <scottdgaines@gmail.com>
2022-08-29 17:38:08 -0600

    Add method returnAllTimeHydration

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,9 +1,21 @@
 class Hydration {
     constructor(hydrationObject) {
-        this.user = hydrationObject.id,
+        this.userID = hydrationObject.userID,
         this.date = hydrationObject.date,
-        this.ouncesConsumed = hydrationObject.numOunces
+        this.numOunces = hydrationObject.numOunces
     };
+
+    returnAllTimeHydration(array, userID) {
+        const newArray = array.filter(element =>
+            element.userID === userID
+        )
+        const userTotal = newArray.reduce((totalOunces, day) => {
+            totalOunces += day.numOunces;
+            return totalOunces
+        }, 0)
+      let userAverage = userTotal / newArray.length;
+      return userAverage
+    }
 };
 
 export default Hydration
