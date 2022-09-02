e033086a905db56f859b72f6d4e4c8c304a1a479
Marianne Barton <mhood24@gmail.com>
2022-09-02 14:20:54 -0600

    Populate chart format to DOM

--- src/scripts.js
+++ src/scripts.js
@@ -22,7 +22,7 @@ import './images/friendIcon.svg';
 import UserRepository from './UserRepository';
 import User from './User';
 import fetchData from './apiCalls.js';
-import { Chart } from 'chart.js';
+import Chart from 'chart.js/auto';
 
 
 //GLOBAL VARIABLES:
@@ -189,7 +189,47 @@ function showDataContainer() {
 function weeklyDataMessage(array, neededData, user){
 user.returnUserWeekData(array, neededData).forEach(array =>
   weekInfoText.innerText += ` ${array}, `)
+  renderWeeklyChart(array)
 }
+
+function renderWeeklyChart(data){
+  const ctx = document.getElementById('myChart');
+  const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+}
+
 
 
 //click on a water/sleep/activity icon populates the larger bubbles with
